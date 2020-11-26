f26a0f093a1a01e11769dad4075d92ebdffd0825
Kevin "Kenny" Niehage <kenny@syseleven.de>
2020-11-26 13:57:32 +0100

    rename index variables

--- bandcamp.user.js
+++ bandcamp.user.js
@@ -8,15 +8,15 @@
 
 // grab Bandcamp data
 var scriptTags = document.getElementsByTagName("script");
-for (var index = 0; index < scriptTags.length; index++) {
-  var temp = scriptTags[index].getAttribute("data-tralbum");
+for (var indexA = 0; indexA < scriptTags.length; indexA++) {
+  var temp = scriptTags[indexA].getAttribute("data-tralbum");
   if ((null != temp) && ("" != temp)) {
     albumData = JSON.parse(temp);
 
     // log the MP3 file paths to the console
     if (undefined != albumData.trackinfo) {
-      for (var index = 0; index < albumData.trackinfo.length; index++) {
-        console.log("%c"+albumData.trackinfo[index]["file"]["mp3-128"],
+      for (var indexB = 0; indexB < albumData.trackinfo.length; indexB++) {
+        console.log("%c"+albumData.trackinfo[indexB]["file"]["mp3-128"],
                     "background: #2BAE66FF; color: #FCF6F5FF; font-weight: bold;");
       }
     }
